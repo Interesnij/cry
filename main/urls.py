ee3a63eb70489f11b09f2edc05e2ff4d25a44982
Serg <ochkarik1983@mail.ru@>
2020-05-27 21:52:06 +0300

    J

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import url, include
-from main.views import MainPageView, AdsPageView, CoursesPageView, AnketsPageView, MainPhoneSend
+from main.views import MainPageView
 
 
 urlpatterns = [
 	url(r'^$', MainPageView.as_view(), name="main"),
-	url(r'ads/$', AdsPageView.as_view(), name="ads_main"),
-	url(r'courses/$', CoursesPageView.as_view(), name="courses_main"),
-	url(r'ankets/$', AnketsPageView.as_view(), name="ankets_main"),
-
-	url(r'^phone_send/$', MainPhoneSend.as_view(), name="phone_sendd"),
 ]
