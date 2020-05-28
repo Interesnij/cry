107a809f3587d61341de2ae598ede92e079cb014
Serg <ochkarik1983@mail.ru@>
2020-05-28 21:53:13 +0300

    H

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from blog.views import BlogPostsView, BlogListView, BlogDetailView
 urlpatterns = [
     url(r'^$', BlogPostsView.as_view(), name='blog_posts'),
     url(r'^(?P<cat_name>[\w\-]+)/$', BlogListView.as_view(), name="blog_index"),
-    url(r'^(?P<pk>\d+)/$', BlogDetailView.as_view(), name="blog_detail")
+    url(r'^/read/(?P<pk>\d+)/$', BlogDetailView.as_view(), name="blog_detail")
 ]
--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,5 @@ from news.views import NewsListView, NewsDetailView
 
 urlpatterns = [
     url(r'^$', NewsListView.as_view(), name='news_index'),
-    url(r'^(?P<pk>\d+)/$', NewsDetailView.as_view(), name="news_detail")
+    url(r'read/^(?P<pk>\d+)/$', NewsDetailView.as_view(), name="news_detail")
 ]
